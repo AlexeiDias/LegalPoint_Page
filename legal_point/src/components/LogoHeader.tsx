221266317c1734ad737734aef23e34336e81d047
Alexei <alexeidias@gmail.com>
2025-03-30 15:28:24 -0700

    spacing adjust3

--- legal_point/src/components/LogoHeader.tsx
+++ legal_point/src/components/LogoHeader.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const LogoHeader: React.FC = () => {
   return (
     <header className="pt-[50px] px-[24px] sm:px-[50px] flex justify-center">
-      <div className="relative w-full max-w-[1200px] h-[375px]">
+      <div className="relative w-full max-w-[1200px] aspect-[16/5]">
         <Image
           src="/logo.png"
           alt="Website Logo"
